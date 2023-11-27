eca7c1042e3408921702b8052af863eb2773925e
Fabra <dominik.fabricky@gmail.com>
2023-11-27 21:09:42 +0100

    Přidání obrázků a dat k filmům

--- src/data.js
+++ src/data.js
@@ -1,12 +1,52 @@
+import harryPotter from "./pictures/HarryPotter.avif"
+import avengers from "./pictures/Avengers.jpeg"
+import coco from "./pictures/Coco.jpeg"
+import grinch from "./pictures/Grinch"
+import homeAlone from "./pictures/HomeAlone.jpeg"
+import jobs from "./pictures/Jobs.png"
+import theSocialNetwork from "./pictures/TheSocialNetwork.jpeg"
+
 const data = [
     {
         id: 1,
-        previewPicture: "../pictures/Harry-Potter.avif",
+        previewPicture: harryPotter,
         title: "Harry Potter",
         description: "Harry Potter, sirotek, se dozví, že je čaroděj. Na svém 11. narozeninách přijímá pozvání na Školu čar a kouzel v Bradavicích, kde začíná jeho dobrodružství."
     },
     {
         id: 2,
-        previewPicture: "../pictures/Harry-Potter.avif"
-    }
+        previewPicture: avengers,
+        title: "Avengers",
+        description: "Různí superhrdinové, včetně Iron Mana, Thora a Captaina Ameriky, se spojují, aby zastavili Lokiho a jeho armádu Chitauriho, bránící Zemi, v epické bitvě plné akce a soudržnosti."
+    },
+    {
+        id: 3,
+        previewPicture: coco,
+        title: "Coco",
+        description: "Mladý hudebník, Miguel, pronikne do Země mrtvých, kde se setká s předky a hledá podporu své vášně pro hudbu, bojuje proti rodinným tajemstvím a objevuje důležitost rodinných vazeb."
+    },
+    {
+        id: 4,
+        previewPicture: grinch,
+        title: "Grinch",
+        description: "Grinch, osamělý tvor, plánuje ukrást Vánoce vesnici Whoville, ale setkání s malou Cindy ho učí o lásce a štěstí, což ho přivádí k proměně a odpuštění."
+    },
+    {
+        id: 5,
+        previewPicture: homeAlone,
+        title: "Home Alone",
+        description: "Kevin, osamělý chlapec, omylem zůstane sám doma během Vánoc. Bojuje s neohrožeností, když se postaví zlodějům, a využívá pastí, aby ochránil svůj domov před jejich vpádem."
+    },
+    {
+        id: 6,
+        previewPicture: jobs,
+        title: "Jobs",
+        description: "Film Jobs sleduje životní cestu Stevea Jobse od začátků s Apple až po revoluční inovace. Zaměřuje se na jeho vztahy, genialitu a kontroverzní vůdčí styl, ukazuje jeho vzestupy a pády."
+    },
+    {
+        id: 7,
+        previewPicture: theSocialNetwork,
+        title: "The Social Network",
+        description: "The Social Network mapuje vznik Facebooku Markem Zuckerbergem, jeho právní spory a osobní konflikty. Představuje ambice, zrádnost a cenu úspěchu v digitální éře sociálních médií."
+    },
 ]
