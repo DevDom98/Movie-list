9d487402def5a9b2caf1a56dbf8e9a1c1abb2f92
Fabra <dominik.fabricky@gmail.com>
2023-11-27 17:36:33 +0100

    Plneni dat

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        id: 1,
+        previewPicture: "../pictures/Harry-Potter.avif",
+        title: "Harry Potter",
+        description: "Harry Potter, sirotek, se dozví, že je čaroděj. Na svém 11. narozeninách přijímá pozvání na Školu čar a kouzel v Bradavicích, kde začíná jeho dobrodružství."
+    },
+    {
+        id: 2,
+        previewPicture: "../pictures/Harry-Potter.avif"
+    }
+]
