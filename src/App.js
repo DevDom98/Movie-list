099787bfb802e27d334751270c699a23eff1677e
Fabra <dominik.fabricky@gmail.com>
2023-11-30 17:48:00 +0100

    Import useSate

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import "./styles/App.css"
 import Card from "./components/Card"
 import Navigation from "./components/Navigation"
 
+import { useState } from "react"
+
 const App = () => {
   
+  const [cardID, setCardID] = useState(1)
+
+  const controlNavigation = (number) =>{
+    setCardID(number++)
+  }
+
+
   return(
+
     <div className="content">
       <Card />
       <Navigation />
