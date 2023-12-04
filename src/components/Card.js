f06bbd9d32698a1866a156b6788fc3e156cc2a56
Fabra <dominik.fabricky@gmail.com>
2023-12-04 17:23:15 +0100

    Vypsani pouze jendoho filmu z pole dat.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,36 +4,34 @@ import { useState } from "react"
 const Card = () =>{
 
     const [cards, setCards] = useState(data)
-
-    const loadAllMovies = ()=>{
-        setCards(data)
+    const [currentMovie, setCurrentMovie] = useState(0)
+    let prevIndex = 0
+    const nextMovie = ()=>{
+        
+        setCurrentMovie((prevIndex)=> (prevIndex +1) % cards.length)
+        console.log("Další")
     }
+
+    const currentCard = cards[currentMovie]
+
 
     return<section>
         <div className="cards">
-            
-            {
-                cards.map((card)=>{
-                    const {id, previewPicture, title, description} = card
-
-                    return  <>
-                            <h1 className="movie-count">{card.id} / {cards.length}</h1>
-                            <div className="card-container" key={id}>
-                            <img className="preview-picture" src={previewPicture} alt="Obrázek"/>
-                            <h1 className="title">{title}</h1>
-                            <p className="description">{description}</p>
+                            <h1 className="movie-count">{currentCard.id} / {cards.length}</h1>
+                            <div className="card-container" key={currentCard.id}>
+                            <img className="preview-picture" src={currentCard.previewPicture} alt="Obrázek"/>
+                            <h1 className="title">{currentCard.title}</h1>
+                            <p className="description">{currentCard.description}</p>
                             <div className="buttons">
                                 <button className="btn-like">Líbí se mi</button>
                                 <button className="btn-delete">Vymazat</button>
                             </div>
                     </div>
-                    </>
-                })
-            }
+            
         </div>
         <div className="navigation-container">
             <a href="#">Předchozí</a>
-            <a href="#">Další</a>
+            <a onClick={nextMovie}>Další</a>
         </div>
     </section>
     
