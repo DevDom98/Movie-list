ed44937c71796364abc1bbb6558e9ec8f3ed1e52
Fabra <dominik.fabricky@gmail.com>
2023-12-04 17:42:09 +0100

    Nastavene prepinani filmu a mazani. Vyhazuje error

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import Card from "./components/Card"
 import { useState } from "react"
 
 const App = () => {
-  
-  const [cardID, setCardID] = useState(1)
-
-  /*const nextMovie = () =>{
-    if(cardID > )
-    setCardID(cardID++)
-  }*/
-
-
   return(
 
     <div className="content">
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,34 +3,47 @@ import data from "../data"
 import { useState } from "react"
 const Card = () =>{
 
-    const [cards, setCards] = useState(data)
+    const [movies, setMovies] = useState(data)
     const [currentMovie, setCurrentMovie] = useState(0)
-    let prevIndex = 0
+    
     const nextMovie = ()=>{
         
-        setCurrentMovie((prevIndex)=> (prevIndex +1) % cards.length)
+        setCurrentMovie((prevIndex)=> (prevIndex +1) % movies.length)
         console.log("Další")
     }
+    
+    const prevMovie = () => {
+        setCurrentMovie((prevIndex) => (prevIndex - 1 + movies.length) % movies.length);
+        console.log("Předchozí")
+      };
+    
+    const deleteMovie = () => {
+        const updatedMovies = [...movies];
+        updatedMovies.splice(currentMovie, 1);
+        setMovies(updatedMovies);
+        setCurrentMovie(0);
+        console.log("vymazat")
+      };
 
-    const currentCard = cards[currentMovie]
+    const currentCard = movies[currentMovie]
 
 
     return<section>
         <div className="cards">
-                            <h1 className="movie-count">{currentCard.id} / {cards.length}</h1>
+                            <h1 className="movie-count">{currentCard.id} / {movies.length}</h1>
                             <div className="card-container" key={currentCard.id}>
                             <img className="preview-picture" src={currentCard.previewPicture} alt="Obrázek"/>
                             <h1 className="title">{currentCard.title}</h1>
                             <p className="description">{currentCard.description}</p>
                             <div className="buttons">
                                 <button className="btn-like">Líbí se mi</button>
-                                <button className="btn-delete">Vymazat</button>
+                                <button onClick={deleteMovie} className="btn-delete">Vymazat</button>
                             </div>
                     </div>
             
         </div>
         <div className="navigation-container">
-            <a href="#">Předchozí</a>
+            <a onClick={prevMovie}>Předchozí</a>
             <a onClick={nextMovie}>Další</a>
         </div>
     </section>
