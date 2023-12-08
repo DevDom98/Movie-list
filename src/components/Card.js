1f6e045fe0a2aac39fa9138cda2908767aafff53
Fabra <dominik.fabricky@gmail.com>
2023-12-08 18:44:19 +0100

    Predelani preklikavani filmu

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,41 +7,35 @@ import { FaAngleRight } from "react-icons/fa6";
 
 const Card = () =>{
 
-    const [movies, setMovies] = useState(data)
-    const [currentMovie, setCurrentMovie] = useState(0)
+    const [index, setIndex] = useState(0)
+    const {image, title, description} = data[index]
     
-    const nextMovie = ()=>{
-        
-        setCurrentMovie((prevIndex)=> (prevIndex +1) % movies.length)
-        console.log("Další")
+    const nextMovie = () =>{
+        setIndex((index)=>{
+            const newIndex = index + 1
+            return newIndex
+        })
+    }
+
+    const prevMovie = () => {
+        setIndex((index)=>{
+            const newIndex = index - 1
+            return newIndex
+        })
     }
     
-    const prevMovie = () => {
-        setCurrentMovie((prevIndex) => (prevIndex - 1 + movies.length) % movies.length);
-        console.log("Předchozí")
-      };
-    
-    const deleteMovie = () => {
-        const updatedMovies = [...movies];
-        updatedMovies.splice(currentMovie, 1);
-        setMovies(updatedMovies);
-        setCurrentMovie(0);
-        console.log("vymazat")
-      };
-
-    const currentCard = movies[currentMovie]
 
 
     return<section>
         <div className="cards">
-                            <h1 className="movie-count">{currentCard.id} / {movies.length}</h1>
-                            <div className="card-container" key={currentCard.id}>
-                            <img className="preview-picture" src={currentCard.previewPicture} alt="Obrázek"/>
-                            <h1 className="title">{currentCard.title}</h1>
-                            <p className="description">{currentCard.description}</p>
+                            <h1 className="movie-count">{index + 1} / {data.length}</h1>
+                            <div className="card-container" >
+                            <img className="preview-picture" src={image} alt="Obrázek"/>
+                            <h1 className="title">{title}</h1>
+                            <p className="description">{description}</p>
                             <div className="buttons">
                                 <button className="btn-like">Líbí se mi</button>
-                                <button onClick={deleteMovie} className="btn-delete">Vymazat</button>
+                                <button className="btn-delete">Vymazat</button>
                             </div>
                     </div>
             
--- src/data.js
+++ src/data.js
@@ -9,46 +9,46 @@ import theSocialNetwork from "./pictures/TheSocialNetwork.jpeg"
 const allMovies = [
     {
         id: 1,
-        previewPicture: harryPotter,
+        image: harryPotter,
         title: "Harry Potter",
         description: "Harry Potter, sirotek, se dozví, že je čaroděj. Na svém 11. narozeninách přijímá pozvání na Školu čar a kouzel v Bradavicích, kde začíná jeho dobrodružství."
-    },/*
+    },
     {
         id: 2,
-        previewPicture: avengers,
+        image: avengers,
         title: "Avengers",
         description: "Různí superhrdinové, včetně Iron Mana, Thora a Captaina Ameriky, se spojují, aby zastavili Lokiho a jeho armádu Chitauriho, bránící Zemi, v epické bitvě plné akce a soudržnosti."
     },
     {
         id: 3,
-        previewPicture: coco,
+        image: coco,
         title: "Coco",
         description: "Mladý hudebník, Miguel, pronikne do Země mrtvých, kde se setká s předky a hledá podporu své vášně pro hudbu, bojuje proti rodinným tajemstvím a objevuje důležitost rodinných vazeb."
     },
     {
         id: 4,
-        previewPicture: grinch,
+        image: grinch,
         title: "Grinch",
         description: "Grinch, osamělý tvor, plánuje ukrást Vánoce vesnici Whoville, ale setkání s malou Cindy ho učí o lásce a štěstí, což ho přivádí k proměně a odpuštění."
     },
     {
         id: 5,
-        previewPicture: homeAlone,
+        image: homeAlone,
         title: "Home Alone",
         description: "Kevin, osamělý chlapec, omylem zůstane sám doma během Vánoc. Bojuje s neohrožeností, když se postaví zlodějům, a využívá pastí, aby ochránil svůj domov před jejich vpádem."
     },
     {
         id: 6,
-        previewPicture: jobs,
+        image: jobs,
         title: "Jobs",
         description: "Film Jobs sleduje životní cestu Stevea Jobse od začátků s Apple až po revoluční inovace. Zaměřuje se na jeho vztahy, genialitu a kontroverzní vůdčí styl, ukazuje jeho vzestupy a pády."
     },
     {
         id: 7,
-        previewPicture: theSocialNetwork,
+        image: theSocialNetwork,
         title: "The Social Network",
         description: "The Social Network mapuje vznik Facebooku Markem Zuckerbergem, jeho právní spory a osobní konflikty. Představuje ambice, zrádnost a cenu úspěchu v digitální éře sociálních médií."
-    },*/
+    },
 ]
 
 export default allMovies
